7b56ac6fb16df255545900914666f71e4a921a4f
jqassis14 <j.qassis14@gmail.com>
2023-09-04 21:07:42 -0400

    Adding more resources to main.tf

--- terraformProjectChatGPT/main.tf
+++ terraformProjectChatGPT/main.tf
@@ -26,7 +26,8 @@ resource "azurerm_virtual_network" "WestRGVnet" {
 resource "azurerm_virtual_network_peering" "peerEastToWest" {
     name = "vnetPeering"
     virtual_network_name = azurerm_virtual_network.eastRGVnet.name
-    remote_virtual_network_id = azurerm_virtual_network.WestRGVnet
+    remote_virtual_network_id = azurerm_virtual_network.WestRGVnet.id
+    resource_group_name = azurerm_resource_group.EastRG.name
   
 }
 
@@ -34,20 +35,55 @@ resource "azurerm_virtual_network_peering" "peerWestToEast" {
     name = "WestToEast"
     virtual_network_name = azurerm_virtual_network.WestRGVnet.name
     remote_virtual_network_id = azurerm_virtual_network.eastRGVnet.id
+    resource_group_name = azurerm_resource_group.WestRG.name
   
 }
 
-resource "azurerm_service_plan" "WepAppServiceEast" {
+resource "azurerm_service_plan" "WebAppServiceEast" {
     name = "WebAppServiceEast"
     resource_group_name = azurerm_resource_group.EastRG.name
+    os_type = "Linux"
+    sku_name = "B1"
+    location = azurerm_resource_group.EastRG.location
 }
 
 
-resource "azurerm_service_plan" "WepAppServiceWest" {
+resource "azurerm_service_plan" "WebAppServiceWest" {
     name = "WebAppServiceWest"
     resource_group_name = azurerm_resource_group.WestRG.name
+    os_type = "Linux"
+    sku_name ="B1"
+    location = azurerm_resource_group.WestRG.location
 }
 
-resource "azurerm_" "name" {
+resource "azurerm_linux_web_app" "EastUSWebApp" {
+    name = "EastUSWebApp"
+    location = azurerm_service_plan.WebAppServiceEast.location
+    resource_group_name = azurerm_resource_group.EastRG.name
+    service_plan_id = azurerm_service_plan.WebAppServiceEast.id
+    site_config {
+
+    }
+
+  
+}
+
+
+resource "azurerm_linux_web_app" "WestUSWebApp" {
+    name = "WestUSWebApp"
+    location = azurerm_service_plan.WebAppServiceWest.location
+    resource_group_name = azurerm_resource_group.WestRG.name
+    service_plan_id = azurerm_service_plan.WebAppServiceWest.id
+    site_config {
+        
+    }
+
+  
+}
+
+resource "azurerm_app_service_source_control" "GitLinkEast" {
+    app_id = azurerm_linux_web_app.EastUSWebApp.id
+    repo_url = "https://github.com/jqassis14/Terraform-on-Azure-Architectures/tree/main/WebAppFiles"
+    branch = "master"
   
 }
